5f82225e769c89b97e76e8a5bc9825056f165d85
TyphaineLeGales <typhaineLeGales@protonmail.com>
2023-10-19 14:32:06 +0200

    add glslify and glsl-noise

--- sources/Experience/Utils/CustomMaterial.js
+++ sources/Experience/Utils/CustomMaterial.js
@@ -1,5 +1,5 @@
 import { MeshBasicMaterial} from "three";
-
+// import glsl from 'glslify'
 
 export default class CustomMaterial extends MeshBasicMaterial {
     constructor(params) {
@@ -9,6 +9,8 @@ export default class CustomMaterial extends MeshBasicMaterial {
     }
 
     addUtils (shader) {
+        const snoise4 = glsl`#pragma glslify: snoise4 = require(glsl-noise/simplex/4d)`;
+
         shader.vertexShader = shader.vertexShader.replace('void main() {', [
             'uniform float uTime;',
             'float clampedSine(float t) {',
@@ -17,30 +19,59 @@ export default class CustomMaterial extends MeshBasicMaterial {
             'float random(vec2 st){',
             'return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);',
             '}',
+            snoise4,
             'void main() {',
         ].join('\n'));
     }
     
     applyEffect (shader, effectName) {
+        let patch = []
         if(effectName === 'wavy') {
-            shader.vertexShader = shader.vertexShader.replace('#include <project_vertex>', [
-                'transformed.x += sin(transformed.y*2.0+uTime)*0.5;',
-                "#include <project_vertex>",
-            ].join('\n'));
+            patch =  [
+                'transformed.x += sin(transformed.y*2.0+uTime)*0.5;'
+            ]
+        }
+        if(effectName === 'blowUp') {
+            patch = [
+                'float s = 1.2;',
+                'transformed += normalize(normal)*s*abs(sin(uTime));' 
+            ]
         }
 
+        if(effectName === 'twist') {
+            patch = [
+                'float a = atan(transformed.z, transformed.x);',
+                'float s = 1.5;',
+                'a += transformed.y * sin(uTime)*s ;',
+                'transformed.xz += vec2(cos(a), sin(a))*length(transformed.xz);',
+            ]
+        }
 
-
+        if(effectName === 'noise') {
+            patch = [
+                'float s = 0.1;',
+                'transformed += normalize(normal)*s;',
+                'transformed += snoise(vec4(transformed, uTime))*0.1;'
+            ]
+        }
+        // re-insert the include after the patch
+        patch.push("#include <project_vertex>")
+        shader.vertexShader = shader.vertexShader.replace('#include <project_vertex>', patch.join('\n'));
     }
 
     onBeforeCompile(shader, renderer) {
         super.onBeforeCompile();
-        console.log(shader.vertexShader)
-        console.log(shader.fragmentShader)
+        // console.log(shader.vertexShader)
+        // console.log(shader.fragmentShader)
         shader.uniforms.uTime = { value:0 }
 
+        
+
         this.addUtils(shader)
-        this.applyEffect(shader, 'wavy')
+        // this.applyEffect(shader, 'wavy')
+        // this.applyEffect(shader, 'blowUp')
+        // this.applyEffect(shader, 'twist')
+        this.applyEffect(shader, 'noise')
 
         this.userData.shader = shader
     }
