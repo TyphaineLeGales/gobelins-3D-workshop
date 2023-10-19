36c98aadf4527418bd1b9c957942b044f4e26f72
TyphaineLeGales <typhaineLeGales@protonmail.com>
2023-10-19 11:13:37 +0200

    custom material with shader patching

--- sources/Experience/assets.js
+++ sources/Experience/assets.js
@@ -6,6 +6,8 @@ export default [
         [
             { name: 'lennaTexture', source: '/assets/lenna.png', type: 'texture' },
             { name: 'image', source: '/assets/image4Shader.jpg', type: 'texture' },
+            { name: 'alien', source: 'assets/alien.glb'}, 
+            { name: 'diffuse', source: '/assets/colorMapAlien.png', type:'texture'}
             // { name: 'rock', source: 'assets/Rock.glb'}, 
             // { name: 'character', source: 'assets/mixamoAnimatedChar.glb'},
             // {name: 'matcap', source: 'assets/matcap.jpg', type:'texture'}, 
